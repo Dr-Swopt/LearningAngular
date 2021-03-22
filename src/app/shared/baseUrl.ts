d21f40932727b5789a42e88051d3e8315fd7675f
Enzo <enzo@softwareoptima.com>
2021-03-22 14:41:12 +0800

    ye

--- src/app/shared/baseUrl.ts
+++ src/app/shared/baseUrl.ts
@@ -1,1 +1,2 @@
-export const baseURL = 'https://124.13.174.103:5000/';
+/* export const baseURL = 'https://124.13.174.103:5000/'; */
+export const baseURL = 'https://swopt.com:5000/';
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
 
   production: false,
-  apiRoot: "https://localhost:5001",
+  apiRoot: "https://swopt.com:5000",
 };
 
 /*
